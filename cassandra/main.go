b2fcc4cc806cee684069a1bdd59ec07c05d18364
GiriVardhan <47814845+GiriVardhan@users.noreply.github.com>
2019-12-23 17:58:05 +0530

    Update main.go

--- cassandra/main.go
+++ cassandra/main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gocql/gocql"
 	"fmt"
         "database/sql"
+     
 )
 
 // Session holds our connection to Cassandra
@@ -12,12 +13,31 @@ var db *sql.DB
 
 func init() {
 	var err error
+         cluster := gocql.NewCluster("172.100.0.10")
+         Session, err = cluster.CreateSession()
+         CreateDb()
+         cluster.Keyspace = "clusterdb"
+         Session, err = cluster.CreateSession()
+	 if err != nil {
+		panic(err)
+	  }
+         fmt.Println("cassandra init done")        
+}
 
-	cluster := gocql.NewCluster("172.17.0.4")
-	cluster.Keyspace = "clusterdb"
-	Session, err = cluster.CreateSession()
+
+func CreateDb(){
+     var err error
+     err = Session.Query(`CREATE KEYSPACE IF NOT EXISTS clusterdb with replication = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };`).Exec()
 	if err != nil {
 		panic(err)
-	}
-	fmt.Println("cassandra init done")
+	} else {
+               err = Session.Query("CREATE TABLE IF NOT EXISTS clusterdb.emps ( empid text PRIMARY KEY, first_name text, last_name text, age int );").Exec()
+	       if err != nil {
+		  panic(err)
+	       }
+
+        }
+
+        fmt.Println("cassandra keyspace and tables created")        
+      
 }
